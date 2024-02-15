74197d94c5ce2c2b1d8ebb107dd198d4c047d1fc
Elena Zotova <elena.ztv.disk@yandex.ru>
2024-02-15 22:48:58 +0300

    fix: table body wrap

--- src/components/orderTable/orderTable.tsx
+++ src/components/orderTable/orderTable.tsx
@@ -3,28 +3,30 @@ import styles from "./orderTable.module.scss";
 
 const OrderTable = ({ rowData = [] }) => {
   return (
-    <table className={styles.orderTable}>
-      <thead>
-        <tr>
-          {columns.map((column, index) => (
-            <th key={index}>{column.label}</th>
+<table className={styles.orderTable}>
+  <thead>
+    <tr>
+      {columns.map((column, index) => (
+        <th key={index}>{column.label}</th>
+      ))}
+    </tr>
+  </thead>
+  <tbody>
+    {rowData.length === 0 ? (
+      <tr>
+        <td colSpan={columns.length}>No data</td>
+      </tr>
+    ) : (
+      columns.map((column, columnIndex) => (
+        <tr key={columnIndex}>
+          {rowData.map((row, rowIndex) => (
+            <td key={rowIndex}>{row[column.label]}</td>
           ))}
         </tr>
-      </thead>
-      <tbody>
-        {rowData.length === 0 ? (
-          <td colSpan={columns.length}>No data</td>
-        ) : (
-          columns.map((column, columnIndex) => (
-            <tr key={columnIndex}>
-              {rowData.map((row, rowIndex) => (
-                <td key={rowIndex}>{row[column.label]}</td>
-              ))}
-            </tr>
-          ))
-        )}
-      </tbody>
-    </table>
+      ))
+    )}
+  </tbody>
+</table>
   );
 };
 
