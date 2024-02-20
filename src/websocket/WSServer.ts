5cac0c850bb01243fe039fd86dc2febe0d892151
Elena Zotova <elena.ztv.disk@yandex.ru>
2024-02-20 22:20:12 +0300

    feat: add logic to websocket server

--- src/websocket/WSServer.ts
+++ src/websocket/WSServer.ts
@@ -1,46 +1,125 @@
 import { Server, Client } from "mock-socket";
-import  CurrencyDataManager from "../components/helpers/currencyDataManager";
+import CurrencyDataManager from "../components/helpers/currencyDataManager";
+import {
+  ClientMessageType,
+  ServerMessageType,
+  OrderStatus,
+  Instrument,
+} from "../constants/Enums";
+import { MarketDataUpdate } from "../Models/ServerMessages";
+import Decimal from "decimal.js";
 
 export default class MockWSServer {
   private mockServer: Server;
   private currencyDataManager: CurrencyDataManager;
+  private intervalId: ReturnType<typeof setInterval> | null;
 
   constructor() {
     this.mockServer = new Server("ws://localhost:3000/ws");
     this.mockServer.on("connection", this.handleConnection.bind(this));
     this.currencyDataManager = new CurrencyDataManager();
+    this.intervalId = null;
+  }
+
+  private getResponse(instrument: Instrument, subscriptionId: string) {
+    const currencyData = this.currencyDataManager.getCurrencyData(instrument);
+
+    if (currencyData) {
+      const bid: Decimal = currencyData.bid;
+      const offer: Decimal = currencyData.offer;
+      const minAmount: Decimal = currencyData.minAmount;
+      const maxAmount: Decimal = currencyData.maxAmount;
+
+      const response = {
+        messageType: ServerMessageType.marketDataUpdate,
+        message: {
+          subscriptionId: subscriptionId,
+          instrument: instrument,
+          quotes: [{ bid, offer, minAmount, maxAmount }],
+        } as MarketDataUpdate,
+      };
+
+      return response;
+    }
   }
 
   private handleConnection(socket: Client): void {
     console.log("Mock server connected.");
-    socket.send(JSON.stringify({ message: "Hello, client!" }));
 
-    socket.on('message', (message: string | Blob | ArrayBuffer | ArrayBufferView) => {
-      if (typeof message === 'string') {
-        console.log('Mock server received message:', message);
+    socket.on(
+      "message",
+      (message: string | Blob | ArrayBuffer | ArrayBufferView) => {
+        if (typeof message === "string") {
+          try {
+            const data = JSON.parse(message);
 
-        const data = JSON.parse(message);
-        if (data && data.message.instrument) {
-          const instrumentValue = data.message.instrument;
+            if (data.messageType === ClientMessageType.subscribeMarketData) {
+              console.log("Received market data request:", data);
 
-          const currencyData = this.currencyDataManager.getCurrencyData(instrumentValue);
-          if (currencyData) {
-              const response = {
-                instrument: instrumentValue,
-                currencyData: currencyData
-              };
+              const subsriptionId = "123";
+
+              socket.send(
+                JSON.stringify({
+                  messageType: ServerMessageType.success,
+                  message: {
+                    subscriptionId: subsriptionId,
+                  },
+                })
+              );
+
+              const instrument = data.message.instrument;
+              const response = this.getResponse(instrument, subsriptionId);
               socket.send(JSON.stringify(response));
-          } else {
-            socket.send(JSON.stringify({ error: 'Invalid instrument' }));
+
+              const intervalCallback = () => {
+                const updateResponse = this.getResponse(
+                  instrument,
+                  subsriptionId
+                );
+                socket.send(JSON.stringify(updateResponse));
+              };
+
+              if (this.intervalId) {
+                clearInterval(this.intervalId);
+              }
+
+              this.intervalId = setInterval(intervalCallback, 1700);
+            } else if (data.messageType === ClientMessageType.placeOrder) {
+              console.log("Received place order:", data);
+              const timeoutValue = 10000;
+
+              const orderStatusValues = Object.values(OrderStatus).filter(
+                (value) => typeof value === "number"
+              );
+              const randomIndex =
+                Math.floor(Math.random() * orderStatusValues.length) + 1;
+
+              const response = {
+                messageType: ServerMessageType.executionReport,
+                message: {
+                  id: data.message.id,
+                  updatedDate: new Date(Date.now() + timeoutValue).toLocaleString(),
+                  orderStatus: randomIndex,
+                },
+              };
+
+              setTimeout(() => {
+                socket.send(JSON.stringify(response));
+                console.log("Sent response:", response);
+              }, timeoutValue);
+            }
+          } catch (error) {
+            console.error("Error parsing JSON:", error);
+            socket.send(
+              JSON.stringify({
+                messageType: ServerMessageType.error,
+                reason: "Invalid JSON format",
+              })
+            );
           }
-        } else {
-          socket.send(JSON.stringify({ error: 'Instrument not provided' }));
         }
-      } else {
-        console.log('Received non-string message:', message);
-        socket.send(JSON.stringify({ error: 'Invalid message format' }));
       }
-    });
+    );
   }
 
   stop(): void {
